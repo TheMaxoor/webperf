320fbeb6abbb9bba8be77cd6080c1219620aab16
Stanislas Manscour <smanscour@groupelexpress.fr>
2019-07-19 11:37:10 +0200

    start picture

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,25 +1,24 @@
 import React from 'react';
 
 class Article extends React.Component {
-    render() {
-        const classname =
-            this.props.item.type !== 'image'
-                ? 'wp-small-article'
-                : 'wp-image-article';
+    render(){
+
+        const classname = this.props.item.type !== "image" ? "wp-small-article" : "wp-image-article";
+        const {image, tag, title} = this.props.item;
 
         return (
             <article className={`${classname} wp-article`}>
                 <a href="">
-                    {this.props.item.image && (
-                        <img src={this.props.item.image} />
+                    {image && (
+                        <picture>
+                            {/* <source type="image/webp" srcSet={`${this.props.item.image}.webp`} /> */}
+                            {/* <source type="image/jpeg" srcSet={`${image}-small.jpeg`} /> */}
+                            <img src={`${image}`} alt="alt" />
+                        </picture>
                     )}
                     <div className="wp-article__text">
-                        <span className="wp-article__text__tag">
-                            {this.props.item.tag}
-                        </span>
-                        <h2 className="wp-article__text__title">
-                            {this.props.item.title}
-                        </h2>
+                        <span className="wp-article__text__tag">{tag}</span>
+                        <h2 className="wp-article__text__title">{title}</h2>
                     </div>
                 </a>
             </article>
