c5fe6a72ede2c96fd17db72bc0b2cd667495b958
Stanislas Manscour <smanscour@groupelexpress.fr>
2019-07-19 15:01:30 +0200

    handle webp

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 
+
 class Article extends React.Component {
     render(){
 
         const classname = this.props.item.type !== "image" ? "wp-small-article" : "wp-image-article";
-        const {image, tag, title} = this.props.item;
+        const {photoContent, image, imageSmall, imageSmallWebp, tag, title} = this.props.item;
 
         return (
             <article className={`${classname} wp-article`}>
                 <a href="">
-                    {image && (
+                    {photoContent && (
                         <picture>
-                            {/* <source type="image/webp" srcSet={`${this.props.item.image}.webp`} /> */}
-                            {/* <source type="image/jpeg" srcSet={`${image}-small.jpeg`} /> */}
-                            <img src={`${image}`} alt="alt" />
+                            <source type="image/webp" srcSet={imageSmallWebp} />
+                            <source type="image/jpeg" srcSet={imageSmall} />
+                            <img src={image} alt="alt" />
                         </picture>
                     )}
                     <div className="wp-article__text">
