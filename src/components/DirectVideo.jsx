aac8b9ccbc874bfadb1d6126bb66d5a76d3f8203
Stanislas Manscour <smanscour@groupelexpress.fr>
2019-07-24 10:58:05 +0200

    deconstruct

--- src/components/DirectVideo.jsx
+++ src/components/DirectVideo.jsx
@@ -17,16 +17,16 @@ class DirectVideo extends React.Component {
     }
 
     render(){
-        const element = this.props.element;
+        const { video, date, tag, text } = this.props.element;
         return (
             <li className="wp-direct__body__collection__item">
                 <div>
                     <div className="wp-direct__body__collection__item__video">
-                        <iframe width="295" height="170" src={element.video} frameBorder="0" allow="encrypted-media; picture-in-picture" allowFullScreen></iframe>
+                        <iframe width="295" height="170" src={video} frameBorder="0" allow="encrypted-media; picture-in-picture" allowFullScreen></iframe>
                     </div>
-                    <span className="wp-direct__body__collection__item__date">{element.date}</span>
-                    <span className="wp-direct__body__collection__item__tag">{element.tag}.</span>
-                    <p className="wp-direct__body__collection__item__text" >{element.text}</p>
+                    <span className="wp-direct__body__collection__item__date">{date}</span>
+                    <span className="wp-direct__body__collection__item__tag">{tag}.</span>
+                    <p className="wp-direct__body__collection__item__text" >{text}</p>
                 </div>
             </li>
         )
