d31c25c7f38230583b3a70dda64313dff199ff7e
Stanislas Manscour <smanscour@groupelexpress.fr>
2019-07-24 15:00:35 +0200

    destructur var

--- src/components/DirectVideo.jsx
+++ src/components/DirectVideo.jsx
@@ -17,7 +17,7 @@ class DirectVideo extends React.Component {
     }
 
     render(){
-        const { video, date, tag, text } = this.props.element;
+        const { video, miniatureWebp, date, tag, text } = this.props.element;
         return (
             <li className="wp-direct__body__collection__item">
                 <div>
@@ -25,13 +25,13 @@ class DirectVideo extends React.Component {
                         { !this.state.loadVideo && (
                             <div className="wp-direct__body__collection__item__video__miniature">
                                 <picture>
-                                    <source type="image/webp" srcSet={'/images/' + element.miniatureWebp} />
-                                    <img src={'/images/' + element.miniatureWebp} width="295px" height="170px" onClick={this.handleClickVideo} />
+                                    <source type="image/webp" srcSet={'/images/' + miniatureWebp} />
+                                    <img src={'/images/' + miniatureWebp} width="295px" height="170px" onClick={this.handleClickVideo} />
                                 </picture>
                             </div>
                         )}
                         { this.state.loadVideo && (
-                            <iframe width="295" height="170" src={element.video + "?autoplay=1"} frameBorder="0" allow="encrypted-media; picture-in-picture" allowFullScreen></iframe>
+                            <iframe width="295" height="170" src={video + "?autoplay=1"} frameBorder="0" allow="encrypted-media; picture-in-picture" allowFullScreen></iframe>
                         )}
                     </div>
                     <span className="wp-direct__body__collection__item__date">{date}</span>
